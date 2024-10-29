464890b64bcb52c08e9fc8829252c5ea6bffe79a
SonicIcarus <jacksinnott1994@gmail.com>
2024-10-29 23:34:04 +0000

    C: have pygame window rendering a black window

--- main.py
+++ main.py
@@ -5,9 +5,24 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    events()
+    set_up_screen(screen=pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH)))
+
+def events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+
+def set_up_screen(screen):
+    while True:
+        screen.fill(color="Black")
+        pygame.display.flip()
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
     main()
