18c55d0ae10cf6b4288c7934b568662f13887882
SonicIcarus <jacksinnott1994@gmail.com>
2024-11-04 19:04:09 +0000

    K: asteroids are splitting when large enough into smaller asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,6 @@
-from circleshape import *
+from circleshape import CircleShape
 from constants import *
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -16,3 +17,21 @@ class Asteroid(CircleShape):
 
     def split(self):
         self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        random_angle = random.uniform(20, 50)
+
+        new_velocity1 = self.velocity.rotate(random_angle)
+        new_velocity2 = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        # Create first new asteroid
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = new_velocity1 * 1.2  # Make it faster
+
+        # Create second new asteroid
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2.velocity = new_velocity2 * 1.2  # Make it faster
